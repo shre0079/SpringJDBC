4fbf497876589240eea9b1d99bce06a9dc02a9d0
Shreyash <shreyashshree0079@gmail.com>
2025-08-08 11:29:39 +0530

    Main class update

--- src/main/java/com/shre/SpringJDBC/SpringJdbcApplication.java
+++ src/main/java/com/shre/SpringJDBC/SpringJdbcApplication.java
@@ -11,8 +11,20 @@ import java.util.List;
 @SpringBootApplication
 public class SpringJdbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+        Student s = context.getBean(Student.class);
+        s.setRollNo(109);
+        s.setName("Shreyash");
+        s.setMarks(79);
+
+        StudentService service = context.getBean(StudentService.class);
+
+        service.addStudent(s);
+
+        List<Student> students = service.getStudents();
+        System.out.println(students);
+
+    }
 
 }
